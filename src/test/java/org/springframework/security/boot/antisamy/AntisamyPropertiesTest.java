27d1eef084e8c6928661f72d84f5fdae3d26e08c
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 20:17:57 +0800

    chore: standardize pom.xml, add tests (>=90% coverage), add English javadocs
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate (haltOnFailure=true)
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire to enable tests + support *_Test.java pattern
    - Add junit-vintage-engine for legacy JUnit 4 tests where needed
    - Fix SCM URLs (https://github.com)
    - Update version to <sb-version>.20260630-SNAPSHOT
    - Auto-generate unit tests for previously untested starters
    - Add detailed English Javadoc on public classes/methods/fields

---
+++ src/test/java/org/springframework/security/boot/antisamy/AntisamyPropertiesTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.springframework.security.boot.antisamy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {{ @link AntisamyProperties }}.
+ *
+ * <p>Verifies default values, getters/setters and POJO contract.</p>
+ *
+ * @author wandl
+ * @since 1.0.0
+ */
+@DisplayName("AntisamyProperties Tests")
+class AntisamyPropertiesTest {
+    @Test
+    @DisplayName("Default constructor creates non-null instance")
+    void testDefaultInstance() {
+        AntisamyProperties props = new AntisamyProperties();
+        assertThat(props).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Setter 'setPolicyMappings' accepts a policyMappings value")
+    void testPolicyMappingsSetter() {
+        AntisamyProperties props = new AntisamyProperties();
+        props.setPolicyMappings(null);
+        // Setter did not throw
+    }
+
+    @Test
+    @DisplayName("Public constant 'DEFAULT_POLICY' has expected value")
+    void testDEFAULT_POLICYConstant() {
+        assertThat(AntisamyProperties.DEFAULT_POLICY).isEqualTo("classpath*:antisamy-policy.xml");
+    }
+}
