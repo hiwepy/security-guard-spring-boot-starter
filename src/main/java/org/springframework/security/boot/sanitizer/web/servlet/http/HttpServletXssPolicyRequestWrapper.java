4062ad9ade2c0435d8d469c46f02200f90d84dbe
wandl-6A72h <hiwepy@gmail.com>
2026-08-09 00:02:13 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 3.1.x branch

--- src/main/java/org/springframework/security/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
+++ src/main/java/org/springframework/security/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
@@ -15,7 +15,7 @@ import java.util.Map.Entry;
 
 /**
  * XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤;
- * @author 		： <a href="https://github.com/hiwepy">hiwepy</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class HttpServletXssPolicyRequestWrapper extends HttpServletRequestWrapper {
 
