91abdd2e370917bead3f7c5611c86fd341380b1a
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:57:27 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/csrfguard/CsrfguardProperties.java
+++ src/main/java/org/springframework/security/boot/csrfguard/CsrfguardProperties.java
@@ -22,7 +22,7 @@ import java.util.*;
 
 /**
  * TODO
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class CsrfguardProperties {
 
--- src/main/java/org/springframework/security/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
+++ src/main/java/org/springframework/security/boot/sanitizer/web/servlet/http/HttpServletXssPolicyRequestWrapper.java
@@ -15,7 +15,7 @@ import java.util.Map.Entry;
 
 /**
  * XSS(Cross Site Scripting)，即跨站脚本攻击请求过滤;
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 public class HttpServletXssPolicyRequestWrapper extends HttpServletRequestWrapper {
 
